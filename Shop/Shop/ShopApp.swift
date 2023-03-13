5ae342d951c843f78d68535cc86c9c6678602afc
Ildar Khabibullin <101454100+moraWoo@users.noreply.github.com>
2023-03-13 20:09:36 +0600

    initial commit

---
+++ Shop/Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Ильдар on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
