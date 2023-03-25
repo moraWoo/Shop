11c765c9db862b57197f246bbf1a9f7e867c9147
moraWoo <pkstratech@yandex.ru>
2023-03-25 19:52:54 +0600

    fix ui of flashSaleItem

--- Shop/Shop/Views/MainView/Views/FlashSaleItem.swift
+++ Shop/Shop/Views/MainView/Views/FlashSaleItem.swift
@@ -1,67 +1,5 @@
 import SwiftUI
 
-    //struct FlashSaleItem: View {
-    //    var body: some View {
-    //        HStack {
-    //            VStack(alignment: .leading) {
-    //                Image("forBigItems")
-    //                    .resizable()
-    //                    .background(Color(red: 229/255, green: 233/255, blue: 239/255))
-    //                    .foregroundColor(Color(red: 84/255, green: 85/255, blue: 137/255))
-    //                    .clipShape(Circle())
-    //                    .frame(width: 25, height: 25)
-    //                Spacer()
-    //                Rectangle()
-    //                    .fill(Color(red: 196/255, green: 196/255, blue: 196/255))
-    //                    .frame(width: 50, height: 17)
-    //                    .cornerRadius(8)
-    //                    .overlay(
-    //                        Text("Phones")
-    //                            .foregroundColor(Color(red: 7/255, green: 6/255, blue: 4/255))
-    //                            .customFont(size: 9, weight: .semibold)
-    //                    )
-    //
-    //                Text("New balance sneakers")
-    //                    .customFont(size: 16, weight: .semibold)
-    //                    .foregroundColor(.white)
-    //                    .frame(width: 87)
-    //                Spacer()
-    //                Text("$ 33,00")
-    //                    .customFont(size: 13, weight: .semibold)
-    //                    .foregroundColor(.white)
-    //            }
-    //            Spacer()
-    //        }.padding(.trailing, 10)
-    //            .padding(.bottom, 10)
-    //            .padding(.top, 10)
-    //            .overlay(
-    //                HStack(spacing: 4) {
-    //                    Button(action: {}) {
-    //                        Image(systemName: "heart")
-    //                            .resizable()
-    //                            .padding(8)
-    //                            .background(Color(red: 229/255, green: 233/255, blue: 239/255))
-    //                            .foregroundColor(Color(red: 84/255, green: 85/255, blue: 137/255))
-    //                            .clipShape(Circle())
-    //                            .frame(width: 28, height: 28)
-    //                    }
-    //
-    //                    Button(action: {}) {
-    //                        Image(systemName: "plus")
-    //                            .resizable()
-    //                            .padding(8)
-    //                            .background(Color(red: 229/255, green: 233/255, blue: 239/255))
-    //                            .foregroundColor(Color(red: 84/255, green: 85/255, blue: 137/255))
-    //                            .clipShape(Circle())
-    //                            .frame(width: 35, height: 35)
-    //                    }
-    //                }
-    //                    .padding(.bottom, 10)
-    //                    .padding(.trailing, 10),
-    //                alignment: .bottomTrailing)
-    //    }
-    //}
-
 struct FlashSaleItem: View {
     var body: some View {
         HStack {
@@ -87,12 +25,13 @@ struct FlashSaleItem: View {
                         .customFont(size: 16, weight: .semibold)
                         .foregroundColor(.white)
                         .frame(width: 80)
+                        .padding(.bottom, 10)
                     Text("$ 33,00")
                         .customFont(size: 13, weight: .semibold)
                         .foregroundColor(.white)
                 }
             }.padding(.leading, 8)
-                .padding(.bottom, 8)
+                .padding(.bottom, 12)
                 .padding(.top, 8)
             
             VStack(alignment: .trailing) {
