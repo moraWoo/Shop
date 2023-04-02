ab874fbf09db79d00e942e2a3712126ea55c1267
moraWoo <pkstratech@yandex.ru>
2023-04-02 22:36:57 +0600

    fix

--- Shop/Shop/ShopApp.swift
+++ Shop/Shop/ShopApp.swift
@@ -3,22 +3,19 @@ import Combine
 
 @main
 struct ShopApp: App {
-    
+
     @ObservedObject var appCoordinator: AppCoordinator
-    
+
     init() {
         let dependencies = AppDependencies()
         appCoordinator = AppCoordinator(dependencies: dependencies)
     }
-
     var body: some Scene {
-        
+
         let contentView = appCoordinator.start().environmentObject(appCoordinator)
-        
+
         WindowGroup {
             contentView.environmentObject(appCoordinator)
         }
     }
 }
-
-
